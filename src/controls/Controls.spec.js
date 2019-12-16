a60ad280004b69fe20b798e148c34d30226b5b90
stevedole808 <stevedolelambda@gmail.com>
2019-12-16 10:49:44 -1000

    first test works

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,9 @@
 // Test away!
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Controls from './Controls';
+
+test('renders', () => {
+    render(<Controls/>);
+});
